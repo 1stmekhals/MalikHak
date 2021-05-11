557d58c7177f6584ac8bd4ff4e65d994650fabbc
Murtaza Ekhlas <83766698+1stmekhals@users.noreply.github.com>
2021-05-11 16:59:00 +0430

    Update Data.py

--- Data.py
+++ Data.py
@@ -1,1 +1,2 @@
-print('hi')
+excuse = "\ hi sir, I hope you are doing well.\"So I wanted to add a complete project of mine but unfortunatlly I have left my laptop in office So I will update this project as soon  as I get my Laptop back"
+print(excuse)
